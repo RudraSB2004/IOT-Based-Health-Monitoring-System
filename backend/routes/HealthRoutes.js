f54ca0d40c166d1a03cd73a43327cacdd26e441e
Rudra Shankar Biswas <130428770+RudraSB2004@users.noreply.github.com>
2026-05-14 23:35:09 +0530

    Fix import case for healthController

--- backend/routes/HealthRoutes.js
+++ backend/routes/HealthRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { createHealthData } = require("../controllers/healthController");
+const { createHealthData } = require("../controllers/HealthController");
 
 router.post("/", createHealthData);
 
